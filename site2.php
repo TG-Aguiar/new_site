7752b21304b1f201e6a453172b69b78a0a7c3faf
Tiago Aguiar <firewall.hidden@hotmail.com>
2019-01-01 13:08:31 -0200

    Novo branch para gerencia o novo site da Chaves construções

---
+++ site2.php
@@ -0,0 +1,40 @@
+<DOCTYPE html>
+
+<html lang="pt-br">
+
+<head>
+
+<meta charset="utf-8"/>
+
+</head>
+
+<body>
+
+<div id="principal">
+
+<header id="cabealho">
+
+<img id="logo" src="logo2.jpg"/>
+
+
+</header>
+
+<section id="meio">
+
+</section>
+
+<aside id="lateral">
+
+
+</aside>
+
+
+<footer id="rodape">
+
+</footer>
+
+<div>
+
+</body>
+
+</html>
